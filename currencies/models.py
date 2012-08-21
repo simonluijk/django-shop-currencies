d9247c38bf3ee3f9b5046a4d55762950339e6ded
Simon Luijk <simon@simonluijk.com>
2012-08-21 14:26:13 +0200

    Move model mixins to separate file. Prevents circular import
    Signed-off-by: Simon Luijk <simon@simonluijk.com>

--- currencies/models.py
+++ currencies/models.py
@@ -9,6 +9,8 @@ from polymorphic.polymorphic_model import PolymorphicModel
 from shop.models_bases import BaseCartItem, BaseOrder
 from shop.models_bases.managers import OrderManager
 
+from .model_mixins import CurrenciesCartItemMixin
+
 
 class Currency(models.Model):
     """
@@ -72,46 +74,6 @@ class Country(models.Model):
     def save(self, *args, **kwargs):
         self.code = self.code.upper()
         super(Country, self).save(*args, **kwargs)
-
-
-class CurrenciesProductMixin(object):
-    """
-    A mixin to add multi currency support to your product models.
-    """
-
-    def get_price_in_currency(self, currency=None):
-        """
-        Returns the price in the provided currency.
-
-        There is a template filter to provide the current currency.
-        You can use it like this:
-
-        {% load currencies %}
-        {% price product %}
-        {% price product "USD" %}
-        ...
-        """
-        # NOTE: I have not implemented this method yet as I am using a custom
-        # implementation because my prices are not attached to the product.
-        # An implemention like chrisglass has done here would make sence.
-        # http://bit.ly/tNpESg
-        # Ofcourse you would use the Currency model above.
-        return super(CurrenciesProductMixin, self).get_price()
-
-
-class CurrenciesCartItemMixin(object):
-    """
-    A mixin to add currency support to your cart item models.
-    """
-
-    def get_price(self):
-        """
-        Override CartItem get_price method to return price bassed on
-        self.cart.currency which is set by "currencies.cart_modifiers.CurrenciesModifier"
-        so make sure you have put it in SHOP_CART_MODIFIERS setting.
-        """
-        # NOTE: Should rase a ImproperlyConfigured if currency is not set
-        return self.product.get_price_in_currency(self.cart.currency)
 
 
 class CurrenciesCartItem(CurrenciesCartItemMixin, BaseCartItem):
